d4716437371ba310cde03fbd9bbaf2c61679e8f2
Eiri ONO <onoeiri@EO-MacBook-Air-2.local>
2013-07-02 18:59:20 +0900

    This is initial commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitakalab::Application.config.secret_key_base = '5351ac60f57af8b628443bc35edae07a1a9fbf5c6887dc9157e5a2d68007320a3b64e3e9d6bc5ab0a35a10490c0bd98e19078192f4d2cc484ebe9e80ca85ea5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mitakalab::Application.config.session_store :cookie_store, key: '_mitakalab_session'
